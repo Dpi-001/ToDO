4dbf3d09f754c253bddebe774556c2117da03f7e
Dpi-001 <sudip9845197918@gmail.com>
2025-05-07 13:32:03 +0545

    Update: Gesturedetector

--- lib/postscreen.dart
+++ lib/postscreen.dart
@@ -1,6 +1,7 @@
 // postscreeneg.dart
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/app_routes.dart';
 import 'package:flutter_application_1/model/post.dart';
 
 class Postscreeneg extends StatefulWidget {
@@ -11,10 +12,22 @@ class Postscreeneg extends StatefulWidget {
 }
 
 class _PostscreenegState extends State<Postscreeneg> {
+  List<Post> fetchedPosts = [];
+
   Future<List<Post>> fetchPost() async {
     final response = await Dio().get(
       'https://jsonplaceholder.typicode.com/posts',
     );
+    try {
+      if (response.statusCode == 200) {
+        for (var item in response.data) {
+          fetchedPosts.add(Post.fromMap(item));
+        }
+        return fetchedPosts;
+      }
+    } catch (e) {
+      print(e.toString());
+    }
 
     if (response.statusCode == 200) {
       for (var item in response.data) {
@@ -25,8 +38,6 @@ class _PostscreenegState extends State<Postscreeneg> {
       throw Exception('Failed to load posts');
     }
   }
-
-  List<Post> fetchedPosts = [];
 
   @override
   Widget build(BuildContext context) {
@@ -40,11 +51,27 @@ class _PostscreenegState extends State<Postscreeneg> {
             itemCount: posts.length,
             itemBuilder: (ctx, index) {
               final post = posts[index];
-              return ListTile(
-                leading: Text(post.userId.toString()),
-                trailing: Text(post.id.toString()),
-                title: Text(post.title),
-                subtitle: Text(post.body),
+
+              return GestureDetector(
+                onTap:
+                    () => Navigator.of(context).pushNamed(
+                      AppRoutes.postViewScreen,
+                      arguments: posts[index].id,
+                    ),
+                child: ListTile(
+                  leading: Text(post.userId.toString()),
+                  trailing: Text(post.id.toString()),
+                  title: Text(
+                    post.title,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Text(
+                    post.body,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
               );
             },
           );
