97f3e02e91a74303714b5f6d3e3fbabf134e7f03
Dpi-001 <sudip9845197918@gmail.com>
2025-05-08 22:30:15 +0545

    Update: api fully post code

--- lib/postscreen.dart
+++ lib/postscreen.dart
@@ -12,69 +12,79 @@ class Postscreeneg extends StatefulWidget {
 }
 
 class _PostscreenegState extends State<Postscreeneg> {
-  List<Post> fetchedPosts = [];
-
-  Future<List<Post>> fetchPost() async {
-    final response = await Dio().get(
-      'https://jsonplaceholder.typicode.com/posts',
-    );
+  List<Post> posts = [];
+  fetchPosts() async {
     try {
-      if (response.statusCode == 200) {
-        for (var item in response.data) {
-          fetchedPosts.add(Post.fromMap(item));
-        }
-        return fetchedPosts;
+      posts.clear();
+      final Dio dio = Dio();
+      final response = await dio.get(
+        'https://jsonplaceholder.typicode.com/posts',
+      );
+      for (var post in response.data) {
+        posts.add(Post.fromMap(post));
       }
+      return posts;
     } catch (e) {
       print(e.toString());
-    }
-
-    if (response.statusCode == 200) {
-      for (var item in response.data) {
-        fetchedPosts.add(Post.fromMap(item));
-      }
-      return fetchedPosts;
-    } else {
-      throw Exception('Failed to load posts');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Post Screen')),
-      body: FutureBuilder<List<Post>>(
-        future: fetchPost(),
-        builder: (context, snapshot) {
-          final posts = snapshot.data!;
-          return ListView.builder(
-            itemCount: posts.length,
-            itemBuilder: (ctx, index) {
-              final post = posts[index];
-
-              return GestureDetector(
-                onTap:
-                    () => Navigator.of(context).pushNamed(
-                      AppRoutes.postViewScreen,
-                      arguments: posts[index].id,
-                    ),
-                child: ListTile(
-                  leading: Text(post.userId.toString()),
-                  trailing: Text(post.id.toString()),
-                  title: Text(
-                    post.title,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  subtitle: Text(
-                    post.body,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              );
+      appBar: AppBar(
+        title: const Text('Posts'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.postViewScreen);
             },
-          );
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: fetchPosts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            List<Post> posts = snapshot.data as List<Post>;
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap:
+                      () => Navigator.of(context).pushNamed(
+                        AppRoutes.postViewScreen,
+                        arguments: posts[index].id,
+                      ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                    child: Card(
+                      shadowColor: Colors.black,
+                      child: ListTile(
+                        leading: Text("${index + 1}"),
+                        title: Text(
+                          posts[index].title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        subtitle: Text(
+                          posts[index].body,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+          return const Center(child: Text('No data available'));
         },
       ),
     );
