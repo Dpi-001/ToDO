9c17bbb002da3a20a03dda8cb3cfb209d81d8fab
Dpi-001 <sudip9845197918@gmail.com>
2025-05-08 09:37:18 +0545

    Fetch: Api data

--- lib/post_view_screen.dart
+++ lib/post_view_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/model/post.dart';
 
 class postviewScreen extends StatefulWidget {
   const postviewScreen({super.key});
@@ -8,6 +10,22 @@ class postviewScreen extends StatefulWidget {
 }
 
 class _postviewScreenState extends State<postviewScreen> {
+  Post? post;
+
+  fetchPostDetail() async {
+    int id = ModalRoute.of(context)!.settings.arguments as int;
+    final Dio dio = Dio();
+    try {
+      final response = await dio.get(
+        'https://jsonplaceholder.typicode.com/posts/$id',
+      );
+      post = response.data;
+      return post;
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: const Text('Post View Screen')));
